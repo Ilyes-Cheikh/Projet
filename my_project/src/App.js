6e28f2ab76805e69c536a493d2fc866833110c78
Your Name <you@example.com>
2021-01-12 00:35:36 +0100

    first commit

---
+++ my_project/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React,{useState} from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import Buttons from './Components/Buttons';
+import Buttons2 from './Components/Buttons2';
+import Commencer from './Components/Commencer';
+function App() {
+  const [compteur,setCompteur]=useState(0)
+  const Name=["Gianluigi Buffon","Cristiano Ronaldo","Leonardo Bonucci","Paulo Dybala","Juan Cuadrado","Rodrigo Bentancur","Alvaro Morata"]
+  const [enable,setEnable]=useState(false)
+  return (
+    <div style={{backgroundColor:'black',height:'800PX',width:'100%'}} >
+
+     {Name[compteur] ? <Commencer compteur={compteur} Name={Name} enable={enable} handleClick={()=>setEnable(true)} ></Commencer> :null}
+
+     {Name[compteur] ? <Buttons enable={enable} incrementer={()=>setCompteur(compteur+1)}></Buttons>  :null}
+      <Buttons2> </Buttons2>
+    </div>
+  );
+}
+
+export default App;
